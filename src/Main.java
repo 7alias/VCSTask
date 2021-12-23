b629161fae59a53fb7e47d52663a96b6482bc902
Ilya Maximov <xc.navi@gmail.com>
2021-12-23 12:10:01 +0200

    Branch2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+String text = "branch2";
 
-        System.out.println("branch2");
+        System.out.println("text");
 
     }
 }
